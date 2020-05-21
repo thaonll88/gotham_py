2dc988d82c2fb9f8ef6445e75fec61674e99f396
Thao Nguyen <thaonll88@Thaos-MacBook-Pro.local>
2020-05-21 19:18:30 +0200

    bai tap 2

--- bai2.py
+++ bai2.py
@@ -1,4 +1,17 @@
 hang_ngang = "1, 2, 3, 4, 5"
-hang_doc = " 1\n 2\n 3\n 4\n \5"
+hang_doc = " 1\n 2\n 3\n 4\n 5\n"
+print "tu 1 toi 5 theo hang ngang"
 print hang_ngang
+print "tu 1 toi 5 theo hang doc"
 print hang_doc
+numero_un = "1"
+numero_deux = "2"
+numero_trois = "3"
+numero_quatre = "4"
+numero_cinq = "5"
+print "tu 1 toi 5 theo hang doc cach 2"
+print numero_un
+print numero_deux
+print numero_trois
+print numero_quatre
+print numero_cinq
